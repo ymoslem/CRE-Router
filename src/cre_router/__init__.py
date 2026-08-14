7e6277bc7d1f64e5af562db1b9fa77886ca1aca9
Yasmin Moslem <48152713+ymoslem@users.noreply.github.com>
2026-08-14 16:56:12 +0100

    Release 0.2.1

--- src/cre_router/__init__.py
+++ src/cre_router/__init__.py
@@ -1,6 +1,6 @@
 """Cluster, Route, Escalate: cascaded framework for cost-aware LLM serving."""
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from cre_router.routing import (  # noqa: F401
     ModelStats,
